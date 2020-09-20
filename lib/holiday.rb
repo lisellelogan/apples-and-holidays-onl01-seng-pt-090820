f615629f5ac31994b8c60df91569097b234f39db
Liselle Logan <liselle.logan@gmail.com>
2020-09-20 15:04:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,16 +61,6 @@ def all_supplies_in_holidays(holiday_hash)
   end
 end
 
-def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-  holiday_hash.map do |seasons, event|
-    event.map do |occasions, components|
-      if components.include?("BBQ")
-        occasions
-      end
-    end
-  end
 end
 
 #def all_holidays_with_bbq(holiday_hash)
